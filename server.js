07340dc69520d6bfe877db2043914198e413fafb
Vladimir Greksa <vladimir.greksa@gmail.com>
2018-02-20 20:23:50 +0100

    Staging app

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+var app = express()
+
+//Hbs setup
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+hbs.registerHelper('upperCaseAll', (text) => {
+    return text.toUpperCase()
+})
+
+//Midlewares
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+    let now = new Date().toString()
+    let log = `${now}: ${req.method} ${req.url}`
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to save log.')
+        }
+
+    })
+    next()
+})
+
+app.use((req, res, next) => {
+    res.render('maintanance.hbs', {
+        title: 'Something got wrong',
+        message: 'Please visit us later'
+    })
+})
+
+app.use(express.static(__dirname + '/public'))
+
+//Routes
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMsg: 'Welcome to home page'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        content: 'Some content'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is up on server 3000')
+})
